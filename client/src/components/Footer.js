469c2b3fbda38ad824383c667bf4d8725af4b1d7
clarafmiranda <clarafmiranda.dev@gmail.com>
2020-05-21 18:12:03 +0200

    Footer done

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,22 @@ import React from "react";
 
 const Footer = () => (
   <footer className="footer">
-    <p>Some footer</p>
+    <p>
+      {" "}
+      Made with 💚 by{" "}
+      <a href="https://github.com/yung-ting" target="_blank">
+        Yung-Ting
+      </a>
+      ,{" "}
+      <a href="https://github.com/clarafmiranda" target="_blank">
+        Clara
+      </a>{" "}
+      and{" "}
+      <a href="https://github.com/mariamuga" target="_blank">
+        Maria
+      </a>
+    </p>
+    <p>© 2020</p>
   </footer>
 );
 
