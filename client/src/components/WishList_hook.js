d8452de844b763633e3e2726af93db59f0b7e3f2
yung-ting <>
2020-09-14 10:10:44 +0200

    app cards respoinsive to mobiles

--- client/src/components/WishList_hook.js
+++ client/src/components/WishList_hook.js
@@ -25,15 +25,18 @@ function WishListHook(props){
         .filter((app) => app.wishUser.includes(props.user._id))
         .map(app => {
             return (
-            
-                <div  key={app._id} className="appCard_wish">
-                <Link to={`/apps/${app._id}`}>
-                    <img src={app.logo||appIconPlaceholder} alt="" style={{width:"50px"}}/>
+                <div  key={app._id} className="appCard">
+                    <Link to={`/apps/${app._id}`}>
+                        <img src={app.logo||appIconPlaceholder} alt="" /> 
+                        {/* style={{width:"50px"}} */}
+                    </Link>
+                    <div>
+                    <Link to={`/apps/${app._id}`}>
                         <h3>{app.name}</h3>
-                        <h6>{app.category.name}</h6>
-                </Link>
+                    </Link>
+                    <h6>{app.category.name}</h6>
+                    </div>
                 </div>
-            
             )
         })}
         </section>
