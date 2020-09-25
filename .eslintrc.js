4ea65cd2420ec34367db67a5f12952c168a9ad2a
yung-ting <>
2020-09-25 12:32:53 +0200

    configure eslint to server and client

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+  },
+  // ignore checking client folder
+  ignorePatterns: [
+    'client/*',
+  ],
+};
---
+++ client/.eslintrc.js
@@ -0,0 +1,24 @@
+// Careful! eslint version is currently higher than react-script,
+// It's temporarily fixed by adding a condition in .env
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+};
