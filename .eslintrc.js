aedf5eb4719b8ed13747d5f1c505dfb36e0ab2ea
yung-ting <>
2020-09-29 12:20:56 +0200

    add linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     ecmaVersion: 12,
   },
   rules: {
+    'no-underscore-dangle': 0,
   },
   // ignore checking client folder
   ignorePatterns: [
--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -19,7 +19,19 @@ module.exports = {
   plugins: [
     'react',
   ],
+  // parser: 'babel-eslint',
   rules: {
+    'no-underscore-dangle': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'import/no-named-as-default': 0,
+    'import/no-named-as-default-member': 0,
+    'no-alert': 0,
+    'no-console': 0,
+    'global-require': 0,
+    'react/prefer-stateless-function': 0,
+    'react/prop-types': 0,
+    'react/destructuring-assignment': 0,
+    'import/prefer-default-export': 0,
   },
   ignorePatterns: [
     'build/*',
