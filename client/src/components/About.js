fa47d834519c357361409b846bcfda09b508f550
yung-ting <>
2020-10-10 15:12:32 +0200

    Add About page and adjust wish list styling

---
+++ client/src/components/About.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './About.scss';
+import './HomePage.scss';
+import './AppDetail.scss'
+
+const About = () => (
+    <main id='appDetail'>
+        <div >
+            <h1>About</h1>
+                <h2>Openstock is a open source platform helps you find free and secure apps, namely open source alternatives. 
+                </h2> 
+                <h2>
+                This is maintained by Yungting, with consultant by xxx in Berlin. 
+                </h2>
+            
+            <h1>Free and Open-Source Software.</h1>
+                <h2> For software, free not only means 'free of charge' but also 'free of ...'. The term “free” indicates that the software does not have constraints on copyrights. The term “open source” indicates the software is in its project form, enabling easy software development from expert developers collaborating worldwide without any need for reverse engineering.
+                We share what we use and do the reseach on it. 
+                </h2>
+                <h2>
+                Feel free to reach out to us at xxx. Or, at Github.
+                </h2>
+        </div>
+    </main>
+)
+
+export default About;
+
