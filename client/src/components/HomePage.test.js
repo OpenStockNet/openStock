955b59741ae6eeafa0f22026bdf058fd09c28290
yung-ting <>
2020-10-10 18:15:22 +0200

    Install testing libraries and run first testing

---
+++ client/src/components/HomePage.test.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import { getQueriesForElement } from '@testing-library/dom';
+
+import HomePage from './HomePage';
+
+const title = `If you don't protect your privacy, who will?`;
+const subTitle = `Find the right app to protect your privacy with OpenStock`
+
+it('renders a title', () => {
+    const { getByText } = render(<HomePage />);
+    expect(getByText(title)).toBeInTheDocument();
+  });
+
+it('renders a subTitle', () => {
+const { getByText } = render(<HomePage />);
+expect(getByText(subTitle)).toBeInTheDocument();
+});
