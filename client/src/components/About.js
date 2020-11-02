8855e0e5d0a26b09ed049d47df8fa585f1601a82
yung-ting <>
2020-11-02 13:51:47 +0100

    Add about page links on navbar and side drawer

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -6,22 +6,22 @@ const About = () => (
   <main id="about">
     <div>
 
-      <h1>
+      <h1 id="about-aTag">
         Openstock is a platform helps you find apps that respect your privacy.
       </h1>
       {' '}
       <h2>
 
-        {'For software, free doesn\'t\ only mean \'free of charge\' but also \'freedom\'. We look into the sources of software that claims to be \'privacy-concerned\', and share those we use and trust.'}
+        {'For software, free doesn\'t only mean \'free of charge\' but also \'freedom\'. We look into the sources of software that claim to be \'privacy-concerned\', and share those we use and trust.'}
 
       </h2>
       <h2>
-        For suggestions and contributions, reach out at
+        To reach out, find us at
         {' '}
-        <a href="mailto:hello@yung-ting-chang.eu">hello@yung-ting-chang.eu</a>
+        <a id="about-aTag" href="mailto:hello@yung-ting-chang.eu">email</a>
         {' '}
         or
-        <a href="https://github.com/OpenStockNet/openStock" target="_blank" rel="noreferrer noopener">
+        <a id="about-aTag" href="https://github.com/OpenStockNet/openStock" target="_blank" rel="noreferrer noopener">
           {' '}
           Github.
         </a>
