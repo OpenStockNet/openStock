b315c8a0ee55af4181e80a5c3667e9d6d8533d86
yung-ting <>
2020-11-16 10:45:15 +0100

    Apply popup modal to textArea

--- client/src/components/TextArea.js
+++ client/src/components/TextArea.js
@@ -2,9 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { addReview, fetchReviews } from '../services/review';
 import './TextArea.scss';
 
+import PopupModal from './PopupModal';
+
 const TextArea = (props) => {
   const [reviewInput, setReviewInput] = useState('');
   const [reviews, setReviews] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [openMsg, setOpenMsg] = useState(null);
+
   const appId = props.app._id;
   const { userId } = props;
 
@@ -12,6 +17,7 @@ const TextArea = (props) => {
     fetchReviews(appId)
       .then((reviewsOfApp) => {
         setReviews(reviewsOfApp);
+        
       })
       .catch((error) => {
         alert(error.message);
@@ -26,13 +32,20 @@ const TextArea = (props) => {
     setReviewInput(event.target.value);
   }
 
+  // popover 
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+const confirm = open ? 'simple-popover' : null;
   function handleSubmit(event) {
     event.preventDefault(); // stops default reloading behaviour
     // make sure take the input value in state, not event.target.value
 
     addReview(reviewInput, appId, userId)
       .then(() => {
-        alert(`Thank you for reviewing ${props.app.name}.`);
+        setOpenMsg('Thanks for sharing!');
+        setOpen(true);
 
         updateReviewsList(appId);
         setReviewInput('');
@@ -46,7 +59,13 @@ const TextArea = (props) => {
 
   return (
     <div id="rateApp">
-      <h4>Reviews</h4>
+      <PopupModal 
+        id= {confirm}
+        open = {open}
+        handleClose = {handleClose}
+        message = {openMsg}
+        />
+      <h4>Comments</h4>
       {reviews.map((review) => (
         <div key={review._id} className="reivews-container">
           <div className="review-user-container">
@@ -65,7 +84,7 @@ const TextArea = (props) => {
           placeholder="How do you like this app?"
           className="comment-text-area"
         />
-        <button type="submit" className="small-submit-btn">Submit</button>
+        <button type="submit" className="small-submit-btn">Post</button>
       </form>
     </div>
   );
