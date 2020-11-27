c634a9b3f43bd09f4f91e6141f121381da608f36
yung-ting <>
2020-11-27 12:18:36 +0100

    Redesign About page and minus changes on content

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -4,27 +4,28 @@ import './HomePage.scss';
 
 const About = () => (
   <main id="about">
-    <div>
+    <div className="about-container">
 
       <h1 id="about-aTag">
-        Openstock is a platform helps you find apps that respect your privacy.
+        Openstock is a platform helping you find apps that respect your privacy.
       </h1>
       {' '}
       <h2>
 
-        {'For software, free doesn\'t only mean \'free of charge\' but also \'freedom\'. We look into the sources of software that claim to be \'privacy-concerned\', and share those we use and trust.'}
+        {'For software, free doesn\'t only mean \'free of charge\' but also means \'freedom\'. We share the software we use and trust, and welcome everyone who holds the same concerns to share on this platform.'}
 
       </h2>
       <h2>
-        To reach out, find us at
+        Happy to connect via
         {' '}
         <a id="about-aTag" href="mailto:hello@yung-ting-chang.eu">email</a>
         {' '}
         or
         <a id="about-aTag" href="https://github.com/OpenStockNet/openStock" target="_blank" rel="noreferrer noopener">
           {' '}
-          Github.
+          Github
         </a>
+        .
       </h2>
     </div>
   </main>
