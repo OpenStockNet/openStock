a95d6741a2e45a4006457b8fd7a18d3d8bca4cc4
yung-ting <>
2020-12-30 17:16:45 +0100

    Change default design of popover

--- client/src/components/Popover.js
+++ client/src/components/Popover.js
@@ -1,22 +1,17 @@
-import React, { useState } from 'react';
-import { logout } from '../services/auth';
+import React, { useState, useEffect } from 'react';
 
 import CloseBtn from './CloseBtn';
 import Backdrop from './Backdrop';
-import SharedSnackbarContext from './SharedSnackbar.context';
-import SharedDialogContext from './SharedDialog.context';
 import confettiIcon from '../images/confetti.svg'
-import './SideDrawer.scss';
 import './Popover.scss';
 
 const PopoverCenter = () => {
     const [show, setShow] = useState(true); 
-    
-    const handlePopoverClose = () => {
-        // popover display: none
-        // backdrop: remove
+
+    function handlePopoverClose() {
         setShow(false);
     }
+   
     let popoverClasses = 'popover-container';
     if (!show) {
         popoverClasses = 'popover-container close';
@@ -26,23 +21,18 @@ const PopoverCenter = () => {
     if (show) {
         backdrop = <Backdrop />;
     }
-    
+
     return (
-        <div>
-            {/* <Backdrop/>
-            <div className='popover-container'>
-                <p>popover text comes here!</p>
-                <button id='small' onClick={handlePopoverClose}>continue the journey</button>
-            </div> */}
+        <div >
             {backdrop}
             <div className={popoverClasses}>
-                <img src={confettiIcon}/>
-                <p>Dear Gerrit, 
-                </p>
-                <p>Thanks for shedding lights on my world to programming in 2020. Would you like to shine through 2021 together?</p>
+            <img src={confettiIcon}/>
+                <div>
+                    <h1>Schascha,</h1>
+                    <p>Thanks for shedding lights on my world to programming in 2020. Would you like to shine through 2021 together?</p>
+                </div>
                 <button className='primary' onClick={handlePopoverClose}>YES, I DO</button>
             </div>
-            
         </div>
     )
 };
