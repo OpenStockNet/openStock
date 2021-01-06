2e9272566245a4ce8d052a8e4dd8d41ef0f1a6a3
yung-ting <>
2021-01-06 15:53:40 +0100

    Change to new year wish text

--- client/src/components/Popover.js
+++ client/src/components/Popover.js
@@ -1,41 +1,38 @@
 import React, { useState, useEffect } from 'react';
-
-import CloseBtn from './CloseBtn';
 import Backdrop from './Backdrop';
-import confettiIcon from '../images/confetti.svg'
+import confettiIcon from '../images/confetti.svg';
 import './Popover.scss';
 
 const PopoverCenter = () => {
-    const [show, setShow] = useState(true); 
+  const [show, setShow] = useState(true);
 
-    function handlePopoverClose() {
-        setShow(false);
-    }
-   
-    let popoverClasses = 'popover-container';
-    if (!show) {
-        popoverClasses = 'popover-container close';
-    }
+  function handlePopoverClose() {
+    setShow(false);
+  }
 
-    let backdrop;
-    if (show) {
-        backdrop = <Backdrop />;
-    }
+  let popoverClasses = 'popover-container';
+  if (!show) {
+    popoverClasses = 'popover-container close';
+  }
 
-    return (
-        <div >
-            {backdrop}
-            <div className={popoverClasses}>
-            <img src={confettiIcon}/>
-                <div>
-                    <h1>Schascha &#x2764;</h1>
-                    <p>Thanks for shedding lights on my world to programming in 2020. Would you like to shine through 2021 together?</p>
-                </div>
-                <button className='primary' onClick={handlePopoverClose}>YES, I DO</button>
-            </div>
+  let backdrop;
+  if (show) {
+    backdrop = <Backdrop />;
+  }
+
+  return (
+    <div>
+      {backdrop}
+      <div className={popoverClasses}>
+        <img src={confettiIcon} />
+        <div>
+          <h1>Happy 2021 &#x2764;</h1>
+          <p>We are on the right track to protect and respect our privacy. Would you like to continue the journey?</p>
         </div>
-    )
+        <button className="primary" onClick={handlePopoverClose}>YES, I DO</button>
+      </div>
+    </div>
+  );
 };
-
 
 export default PopoverCenter;
