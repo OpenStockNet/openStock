4b8b246829abb350211509cef38e682c4c183568
yung-ting <>
2021-01-09 19:49:48 +0100

    Style maginifier icon and search bar

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,35 +1,23 @@
 import React, { useEffect, useState } from 'react';
-
+import IconMagnifier from '../images/searchMagnifier.svg'
 import './Search.scss';
 
 function Search(props) {
-  // const [keys, setKeys] = useState();
-
-  // const handleInputChange = (event) => {
-  //   const keyWords = event.target.value;
-  //   setKeys(keyWords)
-  // }
-
-  // const handleSubmit = () => {
-
-  //   event.preventDefault();
-  //    props.setQuery(keys);
-  // }
   const handleInputChange = (event) => {
     const keyWords = event.target.value;
     props.setQuery(keyWords);
   };
 
   return (
-  // <form id="search" onSubmit={handleSubmit} >
     <form id="search">
       <input
         placeholder="Search an app by name"
         value={props.queries}
-          // value={keys}
         onChange={handleInputChange}
       />
-      {/* <button>find</button> */}
+      <div className='search-icon-container'>
+        <img src={IconMagnifier} alt='search-icon'/>
+      </div>
     </form>
   );
 }
