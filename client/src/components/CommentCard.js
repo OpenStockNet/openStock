86e9f7aeef58b0fcd41f44fd6ffb6708b675c100
yung-ting <>
2021-01-15 12:53:50 +0100

    Add props validation to CommentCard

--- client/src/components/CommentCard.js
+++ client/src/components/CommentCard.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './CommentCard.scss';
 
-function CommentCard(props) {
+function CommentCard({ review }) {
   const timeStampDates = (timeStamp) => timeStamp.slice(0, 10);
 
   return (
     <div className="reviews-container">
       <div className="review-user-container">
-        <h5>{props.review.user.username}</h5>
-        <h5 className="time-stamp">{timeStampDates(props.review.updatedAt)}</h5>
+        <h5>{review.user.username}</h5>
+        <h5 className="time-stamp">{timeStampDates(review.updatedAt)}</h5>
       </div>
-      <p>{props.review.value}</p>
+      <p>{review.value}</p>
     </div>
   );
 }
+
+CommentCard.propTypes = {
+  review: PropTypes.object.isRequired,
+};
 
 export default CommentCard;
