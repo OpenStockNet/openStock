9bfd0d78faec168d1e8bb07a230e629be07859f1
yung-ting <>
2021-01-15 12:49:25 +0100

    Add props validation to CloseBtn

--- client/src/components/CloseBtn.js
+++ client/src/components/CloseBtn.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './CloseBtn.scss';
 
-const closeBtn = (props) => (
-  <div className="close-icon" onClick={props.click} />
+const closeBtn = ({ click }) => (
+  <div className="close-icon" onClick={click} />
 );
+
+closeBtn.propTypes = {
+  click: PropTypes.func.isRequired,
+};
 
 export default closeBtn;
