4fb9df05ee733547702b734af06cbeb38a17e37e
yung-ting <>
2021-01-18 13:02:17 +0100

    Move style.scss to source folder

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import './components/style.scss';
+import './style.scss';
 import './components/Medias.scss';
 
 import App from './App';
