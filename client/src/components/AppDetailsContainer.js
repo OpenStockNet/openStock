6028dff92190bdc149f0019c88dd8d4eb9ab2e99
yung-ting <>
2021-01-18 16:23:06 +0100

    Add title to icons

--- client/src/components/AppDetailsContainer.js
+++ client/src/components/AppDetailsContainer.js
@@ -142,13 +142,13 @@ function AppDetailContainer({ user, match, history }) {
 
   const wishListBtns = (
     app.wishUser.includes(userId)
-      ? <IconButton onHandleWishList={handleRemoveFromWishList} icon={iconApproved} userId={userId} />
-      : <IconButton onHandleWishList={handleAddToWishList} icon={iconPlusSign} userId={userId} />
+      ? <IconButton onHandleWishList={handleRemoveFromWishList} icon={iconApproved} userId={userId} title={'added to wish list'}/>
+      : <IconButton onHandleWishList={handleAddToWishList} icon={iconPlusSign} userId={userId} title={'add to wish list'}/>
   );
 
   const editLinkBtn = (
     <Link to={`/apps/${appId}/edit`} id="small">
-      <img src={iconPencilEdit} alt="" className="icon-btns" />
+      <img src={iconPencilEdit} alt="" className="icon-btns" title="edit"/>
     </Link>
   );
 
--- client/src/components/IconButton.js
+++ client/src/components/IconButton.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './IconButton.scss';
 
-const IconButton = ({ userId, onHandleWishList, icon }) => (
+const IconButton = ({ userId, onHandleWishList, icon, title }) => (
   <button
     type="button"
     key={userId}
     onClick={onHandleWishList}
     id="small"
+    title={title}
   >
     <img src={icon} alt="" className="icon-btns" />
   </button>
