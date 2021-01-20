d173bcb09ff45057d49a098d7c54293a445b3400
yung-ting <>
2021-01-20 13:10:15 +0100

    Add attribution link to icon source

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,29 +4,38 @@ import './Footer.scss';
 
 const Footer = () => (
   <footer className="footer">
+    <div>
+      <p>
+        Made with
+        {' '}
+        <span role="img" aria-label="green-heart">💚</span>
+        {' '}
+        by
+        {' '}
+        <a href="https://github.com/yung-ting" target="_blank" rel="noreferrer noopener">
+          Yung-Ting
+        </a>
+        ,
+        {' '}
+        <a href="https://github.com/clarafmiranda" target="_blank" rel="noreferrer noopener">
+          Clara
+        </a>
+        {' '}
+        and
+        {' '}
+        <a href="https://github.com/mariamuga" target="_blank" rel="noreferrer noopener">
+          Maria
+        </a>
+        {' '}
+        &middot;  © 2020 - 2021
+      </p>
+
+    </div>
     <p>
-      Made with
-      {' '}
-      <span role="img" aria-label="green-heart">💚</span>
-      {' '}
-      by
-      {' '}
-      <a href="https://github.com/yung-ting" target="_blank" rel="noreferrer noopener">
-        Yung-Ting
-      </a>
-      ,
-      {' '}
-      <a href="https://github.com/clarafmiranda" target="_blank" rel="noreferrer noopener">
-        Clara
-      </a>
-      {' '}
-      and
-      {' '}
-      <a href="https://github.com/mariamuga" target="_blank" rel="noreferrer noopener">
-        Maria
-      </a>
+      Icons made by {' '}
+      <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect" target="_blank" rel="noreferrer noopener">Pixel perfect</a>
     </p>
-    <p>© 2020</p>
+
   </footer>
 );
 
