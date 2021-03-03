c6cf9d377637cf7b84a5bcf33de871e5eacfbb2c
yung-ting <48652902+yung-ting@users.noreply.github.com>
2021-03-03 16:34:16 +0100

    Remove node-saas-middleware

--- app.js
+++ app.js
@@ -34,14 +34,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true,
-}));
+// app.use(require('node-sass-middleware')({
+//   src: path.join(__dirname, 'public'),
+//   dest: path.join(__dirname, 'public'),
+//   sourceMap: true,
+// }));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // default value for title local
