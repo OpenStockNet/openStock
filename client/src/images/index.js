6b349a8be03ee1f7f16132ce0d9d3938b547fad5
yung-ting <48652902+yung-ting@users.noreply.github.com>
2021-10-18 19:52:12 +0200

    Upgrade node-sass and react-scripts
    so that they are compatible with node v16.

--- client/src/images/index.js
+++ client/src/images/index.js
@@ -3,17 +3,17 @@
 // to import multiple files in react:
 // create index.js and, import * (all listed files) in the component you need
 
-export const OpenStockLogoIcon = require('./logoOpenstock.svg');
-export const CloudStorageIcon = require('./cloudStorage.svg');
-export const BrowserIcon = require('./browser.svg');
-export const NaviIcon = require('./navigation.svg');
-export const EmailIcon = require('./email.svg');
-export const SearchEngineIcon = require('./searchEngine.svg');
-export const MessengerIcon = require('./messenger.svg');
-export const StreamingIcon = require('./onlineStreaming.svg');
-export const OtherIcon = require('./coffeeCup.svg');
-export const AllAppsIcon = require('./appsAll.svg');
+export const OpenStockLogoIcon = require('./logoOpenstock.svg').default;
+export const CloudStorageIcon = require('./cloudStorage.svg').default;
+export const BrowserIcon = require('./browser.svg').default;
+export const NaviIcon = require('./navigation.svg').default;
+export const EmailIcon = require('./email.svg').default;
+export const SearchEngineIcon = require('./searchEngine.svg').default;
+export const MessengerIcon = require('./messenger.svg').default;
+export const StreamingIcon = require('./onlineStreaming.svg').default;
+export const OtherIcon = require('./coffeeCup.svg').default;
+export const AllAppsIcon = require('./appsAll.svg').default;
 
-export const LoaderIcon = require('./shieldLoader.svg');
-export const SearchIcon = require('./searchMagnifier.svg');
-export const NotFoundIcon = require('./find.svg');
+export const LoaderIcon = require('./shieldLoader.svg').default;
+export const SearchIcon = require('./searchMagnifier.svg').default;
+export const NotFoundIcon = require('./find.svg').default;
